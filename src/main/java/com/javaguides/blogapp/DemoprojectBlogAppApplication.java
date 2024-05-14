d30f5ac76af702b91489d488d30e27317cdf7233
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-15 02:13:38 +0300

    Initial commit

---
+++ src/main/java/com/javaguides/blogapp/DemoprojectBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaguides/blogapp/DemoprojectBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
