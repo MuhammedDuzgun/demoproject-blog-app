3b35b69cfbe8896f0674238f619aba4da53df521
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-16 01:10:24 +0300

    add new controller methods -> PostController.java

--- src/main/java/com/javaguides/blogapp/controller/PostController.java
+++ src/main/java/com/javaguides/blogapp/controller/PostController.java
@@ -5,10 +5,9 @@ import com.javaguides.blogapp.service.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -21,10 +20,37 @@ public class PostController {
         this.postService = postService;
     }
 
+    //create post rest api
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto) , HttpStatus.CREATED);
     }
 
+    // get all posts rest api
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    //get post by id rest api
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    //update post by id rest api
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@PathVariable(name = "id") long id ,
+                                              @RequestBody PostDto postDto) {
+        PostDto responsePost = postService.updatePost(postDto , id);
+        return new ResponseEntity<>(responsePost , HttpStatus.OK);
+    }
+
+    //delete post by id rest api
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post Deleted Successfuly" , HttpStatus.OK);
+    }
 
 }
