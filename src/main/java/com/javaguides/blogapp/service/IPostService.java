959f606f49460b11fe290da3ca8c0819a5ce6640
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-16 01:08:35 +0300

    Add new methods -> IPostService.java

--- src/main/java/com/javaguides/blogapp/service/IPostService.java
+++ src/main/java/com/javaguides/blogapp/service/IPostService.java
@@ -2,6 +2,17 @@ package com.javaguides.blogapp.service;
 
 import com.javaguides.blogapp.dto.PostDto;
 
+import java.util.List;
+
 public interface IPostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto , long id);
+
+    void deletePostById(long id);
+
 }
