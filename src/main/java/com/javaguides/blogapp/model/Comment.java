721d512a609afdd92bc97b4a84359f9806eb92f3
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-17 01:03:45 +0300

    Create Comment model class

---
+++ src/main/java/com/javaguides/blogapp/model/Comment.java
@@ -0,0 +1,27 @@
+package com.javaguides.blogapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id" , nullable = false)
+    private Post post;
+}
