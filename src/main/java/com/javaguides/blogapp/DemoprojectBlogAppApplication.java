1f366c50c6752d13c0791dfd3053742f8d4f94d0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-18 02:42:33 +0300

    Add ModelMapper Bean to project

--- src/main/java/com/javaguides/blogapp/DemoprojectBlogAppApplication.java
+++ src/main/java/com/javaguides/blogapp/DemoprojectBlogAppApplication.java
@@ -1,10 +1,17 @@
 package com.javaguides.blogapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoprojectBlogAppApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoprojectBlogAppApplication.class, args);
