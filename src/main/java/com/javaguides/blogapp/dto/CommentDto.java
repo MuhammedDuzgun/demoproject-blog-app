af29a9d45d27116fa451f98087134c8d0b06682e
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-18 04:14:39 +0300

    Validate Dto Object's Fields

--- src/main/java/com/javaguides/blogapp/dto/CommentDto.java
+++ src/main/java/com/javaguides/blogapp/dto/CommentDto.java
@@ -1,11 +1,22 @@
 package com.javaguides.blogapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Body cannot be empty")
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 }
