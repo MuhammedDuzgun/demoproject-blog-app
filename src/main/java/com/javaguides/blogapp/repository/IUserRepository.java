83195b5b9732053e9a3df19f3f526d03bc34bd91
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-19 04:08:29 +0300

    Add UserRepository, RoleRepository Interfaces

---
+++ src/main/java/com/javaguides/blogapp/repository/IUserRepository.java
@@ -0,0 +1,16 @@
+package com.javaguides.blogapp.repository;
+
+import com.javaguides.blogapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+}
