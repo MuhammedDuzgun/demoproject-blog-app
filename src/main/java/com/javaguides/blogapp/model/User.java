67893d5c19b6b3d619f0da4727a4c038fe7a5c1b
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-20 00:42:32 +0300

    User entity @ManyToMany relationship modified with: fetchType and cascadeType

--- src/main/java/com/javaguides/blogapp/model/User.java
+++ src/main/java/com/javaguides/blogapp/model/User.java
@@ -32,7 +32,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
