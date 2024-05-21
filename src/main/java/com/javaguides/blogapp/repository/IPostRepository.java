75a73c0ab859afa5072491d8adfa888d80b61b8b
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-22 01:40:05 +0300

    Add Custom Method to IPostRepository

--- src/main/java/com/javaguides/blogapp/repository/IPostRepository.java
+++ src/main/java/com/javaguides/blogapp/repository/IPostRepository.java
@@ -3,6 +3,10 @@ package com.javaguides.blogapp.repository;
 import com.javaguides.blogapp.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IPostRepository extends JpaRepository<Post, Long> {
+
+    List<Post>  findByCategoryId(Long categoryId);
 
 }
