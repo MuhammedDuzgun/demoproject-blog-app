441cae59a6685aa7fffc2eb5996ab324402e65bd
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-22 01:39:19 +0300

    Add Category Attribute to Post Entity

--- src/main/java/com/javaguides/blogapp/model/Post.java
+++ src/main/java/com/javaguides/blogapp/model/Post.java
@@ -39,4 +39,8 @@ public class Post {
     )
     private Set<Comment> comments = new HashSet<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
