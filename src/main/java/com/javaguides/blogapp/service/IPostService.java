c81a3f2d0054b09a6d29682d69e78660727fae7c
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-22 01:42:03 +0300

    Add new Method : getPostByCategory and Modify interface

--- src/main/java/com/javaguides/blogapp/service/IPostService.java
+++ src/main/java/com/javaguides/blogapp/service/IPostService.java
@@ -7,13 +7,11 @@ import java.util.List;
 
 public interface IPostService {
     PostDto createPost(PostDto postDto);
-
     PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
-
     PostDto getPostById(long id);
-
     PostDto updatePost(PostDto postDto , long id);
-
     void deletePostById(long id);
+    List<PostDto> getPostsByCategory(Long categoryId);
+
 
 }
