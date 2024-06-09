ecae302e0f4ac5bb297a9eff320af79786ec44c5
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-06-10 01:07:30 +0300

    Modify: SecurityConfig

--- src/main/java/com/javaguides/blogapp/config/SecurityConfig.java
+++ src/main/java/com/javaguides/blogapp/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.javaguides.blogapp.config;
 
 import com.javaguides.blogapp.security.JwtAuthenticationEntryPoint;
 import com.javaguides.blogapp.security.JwtAuthenticationFilter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type =  SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SecurityConfig {
 
     private UserDetailsService userDetailsService;
@@ -52,6 +60,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest()
                                 .authenticated())
                 .exceptionHandling(exception -> exception
