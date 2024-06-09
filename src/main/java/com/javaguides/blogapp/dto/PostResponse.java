254daf948b7fdb7c354f6e565aa6908c46f11c46
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-06-10 01:10:54 +0300

    Add Doc to: PostResponse

--- src/main/java/com/javaguides/blogapp/dto/PostResponse.java
+++ src/main/java/com/javaguides/blogapp/dto/PostResponse.java
@@ -1,19 +1,36 @@
 package com.javaguides.blogapp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Schema(
+        description = "Post Response Field"
+)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostResponse {
+
+    @Schema(description = "List of PostDto")
     private List<PostDto> content;
+
+    @Schema(description = "Page no")
     private int pageNo;
+
+    @Schema(description = "Page size")
     private int pageSize;
+
+    @Schema(description = "Total elements")
     private long totalElements;
+
+    @Schema(description = "Total Pages")
     private int totalPages;
+
+    @Schema(description = "is Last Page of Content")
     private boolean last;
+
 }
